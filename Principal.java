bb4b5fe102bb62d2e709d41d6fee40725a543dff
FDioxF <fernandodioscornoq@hotmail.com>
2022-08-22 09:54:57 -0500

    Segundo commit

--- Principal.java
+++ Principal.java
@@ -8,8 +8,13 @@ public class Principal {
 		boolean fl=false;
 		vida=0;
 		Scanner sc= new Scanner(System.in);
+		System.out.println("Numero?");
+		vida = sc.nextInt();
 		System.out.println("numeró ingresado: "+vida);
 		System.out.println("Hola mundo");
 		
+		for (int i=1; i<=vida; i++)
+			rp +=1;
+		System.out.println("La suma es: "+ rp);
 	}
 }
