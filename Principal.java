bb7a8320907e7119d4f0ecd8098c30a85e990839
FDioxF <fernandodioscornoq@hotmail.com>
2022-08-22 09:42:10 -0500

    Primer Comit

---
+++ Principal.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		int suma;
+		int vida, rp=0;
+		boolean fl=false;
+		vida=0;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("numeró ingresado: "+vida);
+		System.out.println("Hola mundo");
+		
+	}
+}
