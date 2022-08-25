2f4894c62f2958a249bdbc547839a8e486c97cb8
FDioxF <fernandodioscornejo@hotmail.com>
2022-08-24 21:32:31 -0500

    Clase regalo y inplementacion metodo regalo al main

--- Principal.java
+++ Principal.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 
 public class Principal {
 
+    public static String elegir(int vida) {
+        String gift = "";
+        switch (vida) {
+            case 1:
+                gift = "Un pasaje al caribe";
+                break;
+            case 2:
+                gift = "Una visita al museo más cercano a tu casa";
+                break;
+            case 3:
+                gift = "Unsa entrada al cine";
+                break;
+        }
+        return gift;
+    }
+
     public static void main(String[] args) {
         int vida, rp = 0;
         boolean fl = false;
@@ -14,9 +30,12 @@ public class Principal {
             if (rp == 1234)
                 fl = true;
         } while (vida < 3 && !fl);
-        if (fl == true)
+        if (fl == true) {
             System.out.println("Adivinaste!!!!!");
-        else
+            System.out.println("Ganaste!!!!");
+            System.out.println(elegir(vida));
+            elegir(vida);
+        } else
             System.out.println("ups, perdiste...");
     }
 }
